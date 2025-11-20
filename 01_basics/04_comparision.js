69ce163145691ff0842ff2e1a740b37827252d46
parth lakhani <parth.lakhani@tristatetechnology.co.in>
2025-11-20 16:50:47 +0530

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,39 @@
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2+2);
+
+// console.log("02" > 1);
+// console.log("2" > 1);
+
+// console.log("5" > "20");
+
+// console.log(2 < "12");
+console.log("2" < "12");
+console.log("2" > "12");
+console.log("2" == "12");
+
+/*
+When comparing a string with a number, JavaScript will convert the string to a number when doing the comparison. An empty string converts to 0. A non-numeric string converts to NaN which is always false.
+*/
+
+
+// console.log(null == 0);
+// console.log(null > 0);
+// console.log(null >= 0);
+// == and comparision are treat differentaly.
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined >= 0);
+
+
+
+
+
+
+
+
+
+
+
+
