76674230c98fa6b7d60211b62df0807725a02664
parth lakhani <parth.lakhani@tristatetechnology.co.in>
2025-12-03 09:01:04 +0530

    while and do

---
+++ 06_iterations/02_while_do.js
@@ -0,0 +1,124 @@
+// while
+
+
+let i = 1;
+
+
+
+// while (i <= 5) {
+//   console.log(i);
+//   i++;
+// }
+
+
+// while (true) {
+//   console.log("infinite");
+// }
+
+
+// while (true) {
+//   if (i > 5) break;
+//   console.log(i);
+//   i++;
+// }
+
+
+// while (i < 5) {
+//   i++;
+//   if (i === 3) continue;
+//   console.log(i);
+// }
+
+
+// do {
+//   console.log(i);
+//   i++;
+// } while (i <= 5);
+
+
+// while (i < 5) i++;
+
+
+// let j = 10;
+
+// while (i < j) {
+//   console.log(i, j);
+//   i++;
+//   j--;
+// }
+
+
+/*
+
+| While Type              | Description                                      |
+|-------------------------|--------------------------------------------------|
+| Basic while             | Runs while condition is true                     |
+| Infinite while          | Runs forever until break                         |
+| while + break           | Manual exit logic                                |
+| while + continue        | Skip some iterations                             |
+| do...while              | Runs at least once                               |
+| No-body while           | One-line loops                                   |
+| Multi-condition while   | Use multiple variables inside loop               |
+
+*/
+
+
+
+
+// do-while
+
+
+// do {
+//   console.log(i);
+//   i++;
+// } while (i <= 5);
+
+
+
+// let x = 10;
+
+// do {
+//   console.log("Runs once even if condition false");
+// } while (x < 5);
+
+
+// do {
+//   console.log("infinite");
+// } while (true);
+
+
+// do {
+//   if (i > 5) break;
+//   console.log(i);
+//   i++;
+// } while (true);
+
+
+
+// do {
+//   i++;
+//   if (i === 3) continue;
+//   console.log(i);
+// } while (i < 5);
+
+
+
+// do i++; while (i < 5);
+
+
+
+/*
+
+| do…while Type              | Description                                   |
+|----------------------------|-----------------------------------------------|
+| Basic do…while             | Runs at least once; checks after              |
+| Runs once only             | When initial condition is false               |
+| Infinite do…while          | Runs endlessly until break                    |
+| do…while + break           | Manual exit inside loop                       |
+| do…while + continue        | Skip certain iterations                       |
+| No-body do…while           | One-line compact loop                         |
+
+*/
+
+
+
